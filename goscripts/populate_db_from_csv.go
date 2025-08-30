bd2bbf744e5d57d162779740f41fd02b99106a9c
Jan Härkönen <jan.harkonen@gmail.com>
2025-08-30 22:06:59 +0300

    refactor populate function

---
+++ goscripts/populate_db_from_csv.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bufio"
+	"database/sql"
+	"fmt"
+	"os"
+	"strings"
+
+	_ "github.com/lib/pq"
+)
+
+// delete all rows from the crypto_csv_raw_data table and insert the data from the csv file
+func populateDbFromCsv(csvFilePath string) {
+	file, err := os.Open(csvFilePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Load the Postgres URI from environment variables or the .env file
+	postgresURI := os.Getenv("POSTGRES_URI")
+	if postgresURI == "" {
+		fmt.Println("Postgres URI not set in environment variables")
+		file, err := os.Open("../.env")
+		if err != nil {
+			fmt.Println("Error opening .env file:", err)
+			return
+		}
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+			if strings.HasPrefix(scanner.Text(), "postgresql://") {
+				postgresURI = scanner.Text()
+				continue
+			}
+		}
+		defer file.Close()
+	}
+
+	// Open the database connection
+	db, err := sql.Open("postgres", postgresURI)
+	if err != nil {
+		fmt.Println("Error opening database:", err)
+		return
+	}
+	defer db.Close()
+
+	// Delete all rows from table
+	_, err = db.Exec("DELETE FROM piggybank.crypto_csv_raw_data")
+	if err != nil {
+		fmt.Println("Error deleting rows:", err)
+		return
+	}
+
+	total := 0
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	fmt.Println(scanner.Text())
+
+	// Build query for bulk insert
+	bulkQuery := "INSERT INTO piggybank.crypto_csv_raw_data (id, \"timestamp_UTC\", transaction_desc, currency, amount, to_currency, to_amount, native_currency, native_amount, \"native_amount_USD\", transaction_kind, transaction_hash) VALUES"
+
+	for true {
+		ok := scanner.Scan()
+		//if total > 5000 {
+		//break
+		//}
+		if !ok {
+			break
+		}
+		splitRow := strings.Split(scanner.Text(), ",")
+		// descontruct the row into variables
+		id := total
+		timestamp := splitRow[0]
+		transactionDesc := splitRow[1]
+		currency := splitRow[2]
+		amount := splitRow[3]
+		toCurrency := splitRow[4]
+		toAmount := splitRow[5]
+		nativeCurrency := splitRow[6]
+		nativeAmount := splitRow[7]
+		nativeAmountUSD := splitRow[8]
+		transactionKind := splitRow[9]
+		transactionHash := splitRow[10]
+		partialQuery := fmt.Sprintf(" ('%d', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s'),", id, timestamp, transactionDesc, currency, amount, toCurrency, toAmount, nativeCurrency, nativeAmount, nativeAmountUSD, transactionKind, transactionHash)
+		if toAmount == "" {
+			toAmount = "0"
+			partialQuery = fmt.Sprintf(" ('%d', '%s', '%s', '%s', '%s', '%s', NULL, '%s', '%s', '%s', '%s', '%s'),", id, timestamp, transactionDesc, currency, amount, toCurrency, nativeCurrency, nativeAmount, nativeAmountUSD, transactionKind, transactionHash)
+		}
+		bulkQuery += partialQuery
+		total += 1
+		fmt.Println("Total lines:", total)
+	}
+	bulkQuery = bulkQuery[:len(bulkQuery)-1] + ";"
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+	_, err = db.Exec(bulkQuery)
+	if err != nil {
+		fmt.Println("Error executing bulk insert:", err)
+	}
+}
+
+//func addCsvRowToDatabase(db *sql.DB, row string) {
+//	splitRow := strings.Split(row, ",")
+//	if len(splitRow) != 11 {
+//		fmt.Println("Invalid row:", row)
+//		return
+//	}
+//	//timestamp, err := time.Parse(time.RFC3339, splitRow[0])
+//	//if err != nil {
+//	//	fmt.Println("Invalid timestamp:", splitRow[0])
+//	//	return
+//	//}
+//	//timestamp := splitRow[0]
+//	_, err := db.Exec("INSERT INTO piggybank.crypto_csv_raw_data (\"timestamp_UTC\") VALUES($1)", splitRow[0])
+//	if err != nil {
+//		fmt.Println("Error inserting row:", err)
+//		return
+//	}
+//	fmt.Println(splitRow)
+//}
