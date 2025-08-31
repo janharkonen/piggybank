b5f0fee2dab8c6ed0cf65ed5eeaf2816f4cdcc77
Jan Härkönen <jan.harkonen@gmail.com>
2025-08-31 12:26:44 +0300

    fix Scan struct types and implement convert to transaction function

--- goscripts/main.go
+++ goscripts/main.go
@@ -1,18 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"time"
+)
 
 type Transaction struct {
 	Kryptovaluutta          string
-	Aikaleima               string
+	Aikaleima               time.Time
 	Tyyppi                  string // "BUY", "SELL", "CASHBACK", "DIVIDEND"
 	HintaEUR                float64
 	MääräKryptovaluuttana   float64
-	EURKryptovaluutta       float64
+	EURPerKryptovaluutta    float64
 	KryptovaluuttaaJäljellä float64
 	LaskettuOstohinta       float64
 	Voitto                  float64
 	Kommentti               string
+}
+
+type RawDataRow struct {
+	TimestampUTC    time.Time
+	TransactionDesc string
+	Currency        string
+	Amount          float64
+	ToCurrency      string
+	ToAmount        sql.NullFloat64
+	NativeCurrency  string
+	NativeAmount    float64
+	NativeAmountUSD float64
+	TransactionKind string
+	TransactionHash string
+	id              int
 }
 
 func main() {
@@ -27,11 +46,14 @@ func main() {
 	//	fmt.Println("Error initializing database from CSV:", err)
 	//}
 
-	transactions, err := TransformRawData(db)
+	transactions, currencySet, yearList, err := TransformRawData(db)
 	if err != nil {
 		fmt.Println("Error transforming raw data:", err)
 		return
 	}
+	fmt.Println("--------------------------------")
+	fmt.Println(currencySet)
+	fmt.Println(yearList)
 
-	fmt.Println(transactions)
+	fmt.Println(transactions[0])
 }
--- goscripts/transform_raw_data.go
+++ goscripts/transform_raw_data.go
@@ -2,63 +2,95 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 
 	_ "github.com/lib/pq"
 )
 
-type RawDataRow struct {
-	id              string
-	TimestampUTC    string
-	TransactionDesc string
-	Currency        string
-	Amount          float64
-	ToCurrency      string
-	ToAmount        float64
-	NativeCurrency  string
-	NativeAmount    float64
-	NativeAmountUSD float64
-	TransactionKind string
-	TransactionHash string
-}
+type CurrencySet map[string]struct{}
+type YearList []int
 
 // transform the raw data from the database into the transaction struct
-func TransformRawData(db *sql.DB) ([]Transaction, error) {
-	rows, err := db.Query("SELECT * FROM piggybank.crypto_csv_raw_data")
-	transactions := []Transaction{}
+func TransformRawData(db *sql.DB) ([]Transaction, CurrencySet, YearList, error) {
+	// Init variable
+	var transaction Transaction
+	var rawDataRow RawDataRow
+	transactions := make([]Transaction, 0)
+	currencySet := make(CurrencySet)
+	yearList := make(YearList, 0)
 	total := 0
+
+	// Query database
+	rows, err := db.Query("SELECT * FROM piggybank.crypto_csv_raw_data ORDER BY \"timestamp_UTC\" ASC")
 	if err != nil {
-		return []Transaction{}, err
+		return []Transaction{}, nil, nil, err
 	}
 	defer rows.Close()
+
+	// Loop through rows and fill transactions list
 	for rows.Next() {
-		var rawDataRow RawDataRow
 		total += 1
 		fmt.Println("Total rows:", total)
-		var transaction Transaction
-		err := rows.Scan(
-			&rawDataRow.id,
-			&rawDataRow.TimestampUTC,
-			&rawDataRow.TransactionDesc,
-			&rawDataRow.Currency,
-			&rawDataRow.Amount,
-			&rawDataRow.ToCurrency,
-			&rawDataRow.ToAmount,
-			&rawDataRow.NativeCurrency,
-			&rawDataRow.NativeAmount,
-			&rawDataRow.NativeAmountUSD,
-			&rawDataRow.TransactionKind,
-			&rawDataRow.TransactionHash,
-		)
+		rawDataRow, err = convertFromDBtoRawDataRow(rows)
 		if err != nil {
-			return []Transaction{}, err
+			return []Transaction{}, nil, nil, err
 		}
-		transactions = append(transactions, transaction)
-		//if err != nil {
-		//	return []Transaction{}, err
-		//}
-		//transactions = append(transactions, transaction)
+		currencySet[rawDataRow.ToCurrency] = struct{}{}
+
+		transaction, err = convertToTransaction(rawDataRow)
+		if err == nil {
+			transactions = append(transactions, transaction)
+		}
 	}
 
-	return transactions, nil
+	// Construct list of years
+	var firstYear int = transactions[0].Aikaleima.Year()
+	var lastYear int = rawDataRow.TimestampUTC.Year()
+	for year := firstYear; year <= lastYear; year++ {
+		yearList = append(yearList, year)
+	}
+
+	return transactions, currencySet, yearList, nil
+}
+
+func convertFromDBtoRawDataRow(rows *sql.Rows) (RawDataRow, error) {
+	var rawDataRow RawDataRow
+	err := rows.Scan(
+		&rawDataRow.TimestampUTC,
+		&rawDataRow.TransactionDesc,
+		&rawDataRow.Currency,
+		&rawDataRow.Amount,
+		&rawDataRow.ToCurrency,
+		&rawDataRow.ToAmount,
+		&rawDataRow.NativeCurrency,
+		&rawDataRow.NativeAmountUSD,
+		&rawDataRow.TransactionKind,
+		&rawDataRow.TransactionHash,
+		&rawDataRow.NativeAmount,
+		&rawDataRow.id,
+	)
+	return rawDataRow, err
+}
+
+func convertToTransaction(rawDataRow RawDataRow) (Transaction, error) {
+	// Init
+	var transaction Transaction
+
+	// Condition
+	transaction.Aikaleima = rawDataRow.TimestampUTC
+	switch rawDataRow.TransactionKind {
+	case "crypto_purchase":
+		transaction.Tyyppi = "BUY"
+		transaction.Kryptovaluutta = rawDataRow.Currency
+		transaction.HintaEUR = rawDataRow.NativeAmount
+		transaction.MääräKryptovaluuttana = rawDataRow.Amount
+		transaction.EURPerKryptovaluutta = rawDataRow.NativeAmount / rawDataRow.Amount
+		transaction.KryptovaluuttaaJäljellä = rawDataRow.Amount
+	case "crypto_transfer":
+		return Transaction{}, errors.New("not an error per se, the volumes are just too tiny")
+	default:
+		return Transaction{}, errors.New("unknown transaction kind")
+	}
+	return transaction, nil
 }
