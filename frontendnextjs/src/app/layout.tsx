ea57df80bf9458db7610d97d77518591b208d022
Jan Härkönen <jan.harkonen@gmail.com>
2025-09-05 23:39:34 +0300

    make basic frontend template

--- frontendnextjs/src/app/layout.tsx
+++ frontendnextjs/src/app/layout.tsx
@@ -28,6 +28,20 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <footer className="flex-none w-full bg-gray-100 text-gray-800">
+          <div className="mx-auto py-4 px-4 sm:px-6 lg:px-8">
+            <p className="text-center text-sm text-gray-500 hover:text-gray-700">
+              <a 
+                href="https://janharkonen.fi" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="hover:bg-gray-200"
+                >
+                © Jan Härkönen 2025
+              </a>
+            </p>
+          </div>
+        </footer>
       </body>
     </html>
   );
