7d988a757d2e210635f3c408d9142e2d243cc450
Jan Härkönen <jan.harkonen@gmail.com>
2025-09-08 01:25:08 +0300

    make the sign in page nicer

--- frontend_convex_nextjs_clerk/app/page.tsx
+++ frontend_convex_nextjs_clerk/app/page.tsx
@@ -16,48 +16,81 @@ import DarkModeToggle from "@/components/Header/DarkModeToggle";
 export default function Home() {
   return (
     <>
-      <header 
-      className="
-      sticky 
-      top-0 
-      z-10 
-      p-2 
-      border-b-2 
-      bg-slate-100
-      border-slate-300 
-      dark:bg-slate-700
-      dark:border-slate-600 
-      flex 
-      flex-row 
-      gap-2
-      items-center"
-      >
-
-        <span className="text-lg font-bold pl-2 flex-grow">
-          Piggybank
-        </span>
-        <div className="flex-none" ><DarkModeToggle/></div>
-        <div className="flex flex-col items-center" ><UserButton/></div>
-      </header>
-      <main className="p-8 flex flex-col gap-8">
-        <h1 className="text-4xl font-bold text-center">
-          Convex + Next.js + Clerk
-        </h1>
+      <main className="flex flex-col h-screen flex-grow">
         <Authenticated>
+          <Header />
           <Content />
         </Authenticated>
         <Unauthenticated>
+        <div className="
+          w-full
+          h-screen
+          flex
+          justify-center
+          items-center
+        ">
+          <div className="absolute top-0 right-0">
+            <DarkModeToggle />
+          </div>
           <SignInForm />
+        </div>
         </Unauthenticated>
       </main>
     </>
   );
 }
 
+function Header() {
+  return (
+    <header 
+    className="
+    sticky 
+    top-0 
+    z-10 
+    p-2 
+    border-b-2 
+    bg-slate-100
+    border-slate-300 
+    dark:bg-slate-700
+    dark:border-slate-600 
+    flex 
+    flex-row 
+    gap-2
+    items-center"
+    >
+      <span className="text-lg font-bold pl-2 flex-grow">
+        Piggybank
+      </span>
+      <div className="flex-none" ><DarkModeToggle/></div>
+      <div className="flex flex-col items-center" ><UserButton/></div>
+    </header>
+  );
+}
+
 function SignInForm() {
   return (
-    <div className="flex flex-col gap-8 w-96 mx-auto">
-      <p>Log in to see the numbers</p>
+    <div className="
+    w-full
+    max-w-4xl
+    mx-auto
+    shadow-2xl
+    border-2
+    border-mint-200
+    flex 
+    flex-col 
+    rounded-xl 
+    "
+    style={{ background: 'var(--foreground)' }}
+    >
+    <div className="logo-div h-full flex flex-col items-center justify-center"> 
+      <img 
+      src="https://picapi.janharkonen.fi/api/pics/224ad3d792204463bf57ae0eda3bebde.png?BG=92"
+      alt="Piggybank Logo"
+      style={{aspectRatio: 1 / 1, filter: "invert(0)"}}
+      className="logo-img opacity-75 max-h-60 aspect-square"
+
+      />
+      <p className="font-bold text-3xl sm:text-5xl p-4">Piggybank</p>
       <SignInButton mode="modal">
         <button className="bg-foreground text-background px-4 py-2 rounded-md">
           Sign in
@@ -68,6 +101,8 @@ function SignInForm() {
           Sign up
         </button>
       </SignUpButton>
+
+    </div>
     </div>
   );
 }
